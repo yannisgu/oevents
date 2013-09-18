a841e21e7f8cf91220425ae3fb15109f17217d6b
Yannis Guedel <me@yannisguedel.ch>
2013-09-18 21:29:57 +0200

    added openshift env variables

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 var deployd = require('deployd');
 
-
+           /*
 var env = JSON.parse(process.env.VCAP_SERVICES);
  var mongo = env['mongodb-1.8'][0]['credentials'];
-
+              */
 var server = deployd({
-  port: process.env.VCAP_APP_PORT || 5000,
+  port: process.env.OPENSHIFT_NODEJS_PORT || 5000,
   env: 'production',
   db: {
-    host: mongo.hostname,
-    port: mongo.port,
-    name: mongo.db,
+    host: process.env.OPENSHIFT_MONGODB_DB_HOST,
+    port: process.env.OPENSHIFT_MONGODB_DB_PORT,
+    name:  process.env.OPENSHIFT_APP_NAME,
     credentials: {
-      username: mongo.username,
-      password: mongo.password
+      username: process.env.OPENSHIFT_MONGODB_DB_USERNAME,
+      password: process.env.OPENSHIFT_MONGODB_DB_PASSWORD
     }
   }
 });
