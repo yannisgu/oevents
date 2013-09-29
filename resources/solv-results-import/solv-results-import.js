3430ced5f1eaaa059eac47665c008910d3bf5be8
yannisgu <me@yannisguedel.ch>
2013-09-29 19:34:34 +0200

    added missing files

---
+++ resources/solv-results-import/solv-results-import.js
@@ -0,0 +1,184 @@
+var solvBase = "http://www.o-l.ch/cgi-bin/";
+var resultsOld = solvBase + "abfrage?type=rang&event=Auswahl&year="
+var resultsNew = solvBase + "results?type=rang&event=Auswahl&year="
+var async = require('async');
+var ent = require("ent");
+out.log("Begining SOLV Import.");
+ 
+
+var startYear = 2008;
+var endYear = 2013;
+for(var year =  startYear; year <= endYear; year++)
+{
+    importYear(year);
+}
+
+//importResults({url: "abfrage?type=rang&year=2005&event=CO+Vaudoise", year: 2005}, function() {})
+//importYear(2009);
+    
+function importYear(year) {
+    var url;
+    if(year < 2008){
+       url = resultsOld + year;
+    }
+    else {
+        url = resultsNew + year;
+    }
+    
+    request({encoding: null, url: url}, function(error, response, buffer) {
+        var body = "";
+        for (var ii = 0; ii < buffer.length; ii++) {
+                body += String.fromCharCode( buffer.readUInt8(ii));
+        }
+            out.log("result");
+            body = ent.decode(body)
+        if (!error && response.statusCode == 200) {
+            /*var reg = /<select name="event">((\s<option>.*)*)/
+            var result = reg.exec(body);
+            var eventsSelect = result[1]
+            reg = /<option>(.*)/g*/
+            var reg = /<input type=radio name=event value=".*?"> <a href="(.*?)">/g
+           
+            var events = new Array();
+            while(result = reg.exec(body)){
+                events.push({url: result[1], year: year});
+            };
+            
+            async.map(events, importResults, function(err, results){
+                out.log(results)
+            });
+           
+        } 
+        else { 
+            out.error('Error while importing SOLV ' + error);
+        }
+        
+    });
+    
+    
+}
+
+function importResults(options, fn) {
+    var url = solvBase + options.url + "&kind=all";
+     request({encoding: null, url: url}, function(error, response, buffer) {
+        var body = "";
+        for (var ii = 0; ii < buffer.length; ii++) {
+                body += String.fromCharCode( buffer.readUInt8(ii));
+        }
+        body = ent.decode(body)
+        var categories = new Array();
+        
+        var reg = /<b>(.*?)<.b>\s<pre>\( (\d*?.\d*?) km,\s*?(\d*?) m,  ?(\d*?) Po.\)(.*\s*)*?<.pre>/g
+        
+        while(res = reg.exec(body)){
+            categories.push({sourceCode: res[0], name: res[1]});
+            
+        }
+        
+        
+        if(categories.length == 0){
+           // out.log("error reading result categories: " + options.url)
+            fn(null, null);
+        }
+        else{
+            dpd.events.get({source: 'solvResults', urlSource: url}, function(result, err) {
+                var res;
+                if(options.year < 2008){
+                    var regex = /<td valign=top>\s<pre>(.*)/g
+                    var res = regex.exec(body);
+                    var eventLine = res[1].replace(/(<b>|<\/b>|<\/a>|<a href=".*">)/g, "");
+                    var regex;
+                    regex = /(?:<b>)?(?:<a href=".*?">)?((?:[^ <]+\s)*(?:\S*[^ ,<] ))(?:<.a>)? +((?:\S+ )*(?:[^\s,]+))? +(?:((?:\S+\s?)+), )?((?:\d| )\d)\.(.*)(\d\d\d\d)(?:<.b>)*/g
+                    res = regex.exec(eventLine);
+                }
+                else{
+                    regex = /<h2>(.*?)(?:<a href=".*">.*)?<.h2>(?:\s.*){4}\s<tr><td .*?>(?:.*?)<.td><td .*?>(.*?)<.td><td .*?>(?:(.*),) *(\d?\d). *(\S*) *(\d\d\d\d)<.td>/g
+                    res = regex.exec(body)
+                }
+                 //console.log(options.url)
+                    if(!res){
+                        out.error("error reading result page:" + options.url)
+                        fn(null, null)
+                    }
+                    else{
+                         var obj = {}
+                        if(result.length > 0) {
+                            obj.id = result[0].id; 
+                        }
+                        
+                        obj.urlSource= url;
+                        obj.source = 'solvResults';
+                        obj.name = res[1] ? res[1].trim() : null;
+                        obj.map = res[2] ? res[2].trim() : null;
+                        obj.eventCenter = res[3] ? res[3].trim() : null;
+                        if(res[4] && res[5] && res[6]){
+                            obj.date = (new Date(res[6], getMonth(res[5]), res[4])).getTime()
+                        }
+                        //console.log(obj)
+                        dpd.events.post(obj, function(res, err) {
+                            //importResultsOneEvent(res.id);
+                            fn(null, obj)
+                        });
+                        
+                    }
+                    
+                });
+            
+            function importResultsOneEvent(id) {
+                var regex = /((\d+)\.)? (.{22}\w*?) (\d\d)  (.{18,}) (.{19,}) (((\d?\d:)?\d\d:\d\d)|.*)/g
+                async.map(categories, function(category, categoryFn){
+                    dpd.results.get({eventId: id, category: category.name}, function(results, err){
+                        console.log(results)
+                        if(results.length <= 0){
+                            var items = [];
+                            while(res = regex.exec(category.sourceCode)){
+                                var resultItem = {
+                                    eventId: id,
+                                    category: category.name,
+                                    name: res[3].trim(),
+                                    rank: res[2],
+                                    yearOfBirth: res[4],
+                                }
+                                items.push(resultItem)
+                            }
+                            async.map(items, dpd.results.post, function(error, results){
+                                categoryFn(null, "");
+                                
+                            } )
+                            
+                        }
+                        else{
+                            categoryFn(null, "");
+                        }
+                    })
+                },
+                function(err, results){
+                    fn(null, id);
+                })
+                   
+                
+            }
+            
+        }
+     });
+}
+
+var months = {
+    "Jan.": 0,
+    "Feb.": 1,
+    "März": 2,
+    "Apr.": 3,
+    "Mai": 4,
+    "Juni": 5,
+    "Juli": 6,
+    "Aug.": 7,
+    "Sep.": 8,
+    "Okt.": 9,
+    "Nov.": 10,
+    "Dez.": 11
+};
+
+function getMonth(monthString){
+   return months[monthString.trim()];
+}
+
