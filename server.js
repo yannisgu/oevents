cd6465d7cc1fe4aa1ea32aea505fc629f4164775
yannisgu <me@yannisguedel.ch>
2013-10-05 11:35:28 +0200

    added dpd to server.js

--- server.js
+++ server.js
@@ -1,6 +1,31 @@
-var http = require('http')
-var port = process.env.PORT || 1337;
-http.createServer(function(req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello yannis\n');
-}).listen(port);
+#!/usr/bin/env node
+var deployd = require('deployd');
+console.log(process.env.OPENSHIFT_APP_NAME);
+
+
+var server = deployd({
+    port: process.env.PORT || 5000,
+    env: process.env.NODE_ENV || 'production',
+    db: {
+        host: "widmore.mongohq.com",
+        port: 10010,
+        name:  "oevents",
+        credentials: {
+            username: "yannisgu",
+            password: process.env.MONGOHQ_PASSWORD
+        }
+    }
+});
+
+server.listen();
+
+server.on('listening', function() {
+    console.log("Server is listening");
+});
+
+server.on('error', function(err) {
+    console.error(err);
+    process.nextTick(function() { // Give the server a chance to return an error
+        process.exit();
+    });
+});
