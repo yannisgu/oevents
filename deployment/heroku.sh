31d8b01d1560f96ca7f05d346227979b75322acb
yannisgu <me@yannisguedel.ch>
2013-10-12 23:25:08 +0200

    fixed ssh

--- deployment/heroku.sh
+++ deployment/heroku.sh
@@ -32,12 +32,10 @@ git add .
 
 
 heroku git:remote --app oevents
-ssh-keygen -t rsa -f ~/.heroku_key -N '' -I "deploykey@travis"
+ssh-keygen -t rsa -N '' -I "deploykey@travis"
 
-chmod 600 ~/.heroku_key
-ssh-add -D
-ssh-add ~/.heroku_key
-heroku keys:add ~/.heroku_key.pub
+chmod 600 ~/.ssh/id_rsa
+heroku keys:add ~/.ssh/id_rsa.pub
 
 echo "heroku.com,50.19.85.154 ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAu8erSx6jh+8ztsfHwkNeFr/SZaSOcvoa8AyMpaerGIPZDB2TKNgNkMSYTLYGDK2ivsqXopo2W7dpQRBIVF80q9mNXy5tbt1WE04gbOBB26Wn2hF4bk3Tu+BNMFbvMjPbkVlC2hcFuQJdH4T2i/dtauyTpJbD/6ExHR9XYVhdhdMs0JsjP/Q5FNoWh2ff9YbZVpDQSTPvusUp4liLjPfa/i0t+2LpNCeWy8Y+V9gUlDWiyYwrfMVI0UwNCZZKHs1Unpc11/4HLitQRtvuk0Ot5qwwBxbmtvCDKZvj1aFBid71/mYdGRPYZMIxq1zgP1acePC1zfTG/lvuQ7d0Pe0kaw==" >> ~/.ssh/known_hosts
 
