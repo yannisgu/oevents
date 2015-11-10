d4b49d22ec290a8b02dcf984d84fe16802cae36f
Yannis Guedel <me@yannisguedel.ch>
2015-11-10 09:03:05 +0100

    configured azure

---
+++ app.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+var deployd = require('deployd');
+
+
+var server = deployd({
+    port: process.env.PORT || 12334,
+    env: process.env.NODE_ENV || 'production',
+    db: {
+        host: "paulo.mongohq.com",
+        port: 10013,
+        name:  "oevents",
+        credentials: {
+            username: "oevents",
+            password: process.env.MONGOHQ_PASSWORD
+        },
+        options: {auto_reconnect: true}
+    }
+});
+
+server.listen();
+
+server.on('listening', function() {
+    console.log("Server is listening");
+});
+
+server.on('error', function(err) {
+    console.log("error at " + new Date());
+    console.error(err);
+    process.nextTick(function() { // Give the server a chance to return an error
+        process.exit();
+    });
+});
